fc75ead01a608d78d55193e0a67ed8c79f9e8252
Varsha <95171666+varsha2503@users.noreply.github.com>
2024-02-24 00:44:37 +0530

    Update bookModel.js

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -6,7 +6,16 @@ const bookscheme = new mongoose.Schema({
     // Define schema fields with their types and requirements
     name: { type: String, require: true },
     author: { type: String, require: true },
-    isbn: { type: Number, require: true },
+    isbn: { type: Number, require: true,
+           unique: true,
+           validate : { 
+               validator: function (v) {
+                  // Custom ISBN validation function for 12 digits
+                  return /^\d{12}$/.test(v);
+                },
+               message: props => `${props.value} is not a valid ISBN number!`,
+               },
+          },
     des: { type: String, require: true },
     price: { type: Number, require: true },
     image: { type: String, require: true },
